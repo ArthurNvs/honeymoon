9ffd3e5a67de575b76921cc491af5686b8acef2a
Arthur Neves <arthurnvsm@gmail.com>
2021-12-16 09:46:09 -0300

    Project first setup and CardView

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Arthur Neves on 16/12/21.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Honeymoon/Model/DestinationModel.swift
@@ -0,0 +1,15 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Arthur Neves on 16/12/21.
+//
+
+import SwiftUI
+
+struct Destination {
+  var place: String
+  var country: String
+  var image: String
+  
+}
