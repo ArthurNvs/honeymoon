13e974a15f2ae72baf6c10f6c9a5329cce4cc55c
Arthur Neves <arthurnvsm@gmail.com>
2021-12-16 17:14:25 -0300

    Show alert when booking

--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State var showAlert = false
+  @State var showGuide = false
+  
   var body: some View {
     VStack {
       HeaderView()
@@ -19,8 +22,14 @@ struct ContentView: View {
       
       Spacer()
       
-      FooterView()
-    }
+      FooterView(showBookingAlert: $showAlert)
+    } //: VStack
+    .alert(isPresented: $showAlert) {
+      Alert(
+        title: Text("We booked for you!"),
+        message: Text("Wishing a lovely and most precious of the times together for the amazing couple."),
+        dismissButton: .default(Text("Happy Honeymoon!")))
+    } //: alert
   }
 }
 
--- Honeymoon/View/FooterView.swift
+++ Honeymoon/View/FooterView.swift
@@ -8,39 +8,42 @@
 import SwiftUI
 
 struct FooterView: View {
-    var body: some View {
-      HStack {
-        Image(systemName: "xmark.circle")
-          .font(.system(size: 42, weight: .light))
-        
-        Spacer()
-        
-        Button {
-          print("success")
-        } label: {
-          Text("Book Destination".uppercased())
-            .font(.system(.subheadline, design: .rounded))
-            .fontWeight(.heavy)
-            .padding(.horizontal, 20)
-            .padding(.vertical, 12)
-            .accentColor(Color.pink)
-            .background(
-              Capsule().stroke(Color.pink, lineWidth: 2)
-            )
-        } //: Button
-        
-        Spacer()
-        
-        Image(systemName: "heart.circle")
-          .font(.system(size: 42, weight: .light))
-      }
-      .padding()
+  @Binding var showBookingAlert: Bool
+  
+  var body: some View {
+    HStack {
+      Image(systemName: "xmark.circle")
+        .font(.system(size: 42, weight: .light))
+      
+      Spacer()
+      
+      Button {
+        self.showBookingAlert.toggle()
+      } label: {
+        Text("Book Destination".uppercased())
+          .font(.system(.subheadline, design: .rounded))
+          .fontWeight(.heavy)
+          .padding(.horizontal, 20)
+          .padding(.vertical, 12)
+          .accentColor(Color.pink)
+          .background(
+            Capsule().stroke(Color.pink, lineWidth: 2)
+          )
+      } //: Button
+      
+      Spacer()
+      
+      Image(systemName: "heart.circle")
+        .font(.system(size: 42, weight: .light))
     }
+    .padding()
+  }
 }
 
 struct FooterView_Previews: PreviewProvider {
-    static var previews: some View {
-        FooterView()
-        .previewLayout(.fixed(width: 375, height: 80))
-    }
+  @State static var showAlert: Bool = false
+  static var previews: some View {
+    FooterView(showBookingAlert: $showAlert)
+      .previewLayout(.fixed(width: 375, height: 80))
+  }
 }
